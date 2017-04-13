f5d5b5a528e0546c2ff966233e28aacffcde4fed
Bruce Mitchener <bruce.mitchener@gmail.com>
2017-04-13 17:24:48 +0700

    Add skeleton of gestures library.

---
+++ gestures/src/lib.rs
@@ -0,0 +1,20 @@
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! Trencadis Gestures
+//!
+//! The hope is that this quickly becomes a stand-alone crate
+//! and can be widely used.
+
+#![warn(missing_docs)]
+#![deny(trivial_numeric_casts, unstable_features,
+        unused_import_braces, unused_qualifications)]
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {}
+}
